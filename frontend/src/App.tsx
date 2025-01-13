793d37b27459868b0654a62ae2c962b794272811
Yaseen KC <ymkcmpd@gmail.com>
2025-01-13 21:03:05 +0530

    (ui-componet): add card component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,39 @@
 import "./App.css";
-import { Button } from "./components/button";
+import { Button } from "./components/Button";
+import { Card } from "./components/Card";
 import { PlusIcon } from "./icons/PlusIcon";
 import { ShareIcon } from "./icons/ShareIcon";
 
 function App() {
   return (
-    <div>
-      <Button
-        varient="primary"
-        text="Add content"
-        startIcon={<PlusIcon />}
-      ></Button>
+    <div className="p-4">
+      <div>
+        <Button
+          varient="primary"
+          text="Add content"
+          startIcon={<PlusIcon />}
+        ></Button>
 
-      <Button
-        varient="secondary"
-        text="Share Brain"
-        startIcon={<ShareIcon />}
-      ></Button>
+        <Button
+          varient="secondary"
+          text="Share Brain"
+          startIcon={<ShareIcon />}
+        ></Button>
+      </div>
+
+      <div className="flex gap-4">
+        <Card
+          type="twitter"
+          link="https://twitter.com/yaseeenkc/status/1861410120858771756"
+          title="First tweet"
+        />
+
+        <Card
+          type="youtube"
+          link="https://www.youtube.com/watch?v=xYO6otF0xeM"
+          title="First video"
+        />
+      </div>
     </div>
   );
 }
--- frontend/src/icons/index.ts
+++
@@ -1,9 +0,0 @@
-export interface IconProps {
-    size: "sm" | "md" | "lg"
-}
-
-export const iconSizeVariants = {
-    "sm": "size-2",
-    "md": "size-4",
-    "lg": "size-6",
-}
