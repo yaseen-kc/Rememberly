e03ee6aba3cb863274faa8e9957fa214941922ac
Yaseen KC <ymkcmpd@gmail.com>
2025-01-15 15:16:18 +0530

    (component):create sidebar ui component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,50 +1,56 @@
 import { useState } from "react";
 import "./App.css";
+
 import { Button } from "./components/Button";
 import { Card } from "./components/Card";
+
 import { CreateContentModel } from "./components/CreateContentModel";
+import { Sidebar } from "./components/Sidebar";
 import { PlusIcon } from "./icons/PlusIcon";
 import { ShareIcon } from "./icons/ShareIcon";
 
 function App() {
-  const [modelOpen, setModelOpen] = useState(true);
+  const [modelOpen, setModelOpen] = useState(false);
   return (
-    <div className="p-4">
-      <CreateContentModel
-        open={modelOpen}
-        onClose={() => {
-          setModelOpen(false);
-        }}
-      />
-      <div className=" flex justify-end gap-4">
-        <Button
-          onClick={() => {
-            setModelOpen(true);
+    <div>
+      <Sidebar />
+      <div className="p-4 ml-72 min-h-screen bg-gray-100">
+        <CreateContentModel
+          open={modelOpen}
+          onClose={() => {
+            setModelOpen(false);
           }}
-          varient="primary"
-          text="Add content"
-          startIcon={<PlusIcon />}
-        ></Button>
-
-        <Button
-          varient="secondary"
-          text="Share Brain"
-          startIcon={<ShareIcon />}
-        ></Button>
-      </div>
-
-      <div className="flex gap-4">
-        <Card
-          type="twitter"
-          link="https://twitter.com/yaseeenkc/status/1861410120858771756"
-          title="First tweet"
         />
+        <div className=" flex justify-end gap-4">
+          <Button
+            onClick={() => {
+              setModelOpen(true);
+            }}
+            varient="primary"
+            text="Add content"
+            startIcon={<PlusIcon />}
+          ></Button>
 
-        <Card
-          type="youtube"
-          link="https://www.youtube.com/watch?v=xYO6otF0xeM"
-          title="First video"
-        />
+          <Button
+            varient="secondary"
+            text="Share Brain"
+            startIcon={<ShareIcon />}
+          ></Button>
+        </div>
+
+        <div className="flex gap-4">
+          <Card
+            type="twitter"
+            link="https://twitter.com/yaseeenkc/status/1861410120858771756"
+            title="First tweet"
+          />
+
+          <Card
+            type="youtube"
+            link="https://www.youtube.com/watch?v=xYO6otF0xeM"
+            title="First video"
+          />
+        </div>
       </div>
     </div>
   );
---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,7 @@
+export function Sidebar() {
+  return (
+    <div className="h-screen bg-white border-r w-72 fixed left-0 top-0">
+      Hi there
+    </div>
+  );
+}
