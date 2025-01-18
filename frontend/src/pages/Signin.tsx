1a9bb8d3184c5c2a3672552f9b821135229b9c27
Yaseen KC <ymkcmpd@gmail.com>
2025-01-18 18:05:00 +0530

    (page):create signup page

---
+++ frontend/src/pages/Signin.tsx
@@ -0,0 +1,21 @@
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+
+export default function Signin() {
+  return (
+    <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
+      <div className="bg-white rounded-xl border min-w-48 p-8">
+        <Input placeholder="Username" />
+        <Input placeholder="Password" />
+        <div className="flex justify-center pt-4">
+          <Button
+            laoding={false}
+            varient="primary"
+            text="Signin"
+            fullWidth={true}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
