86bb354ad57e4f466d31e142bed9f97918fe163d
Yaseen KC <ymkcmpd@gmail.com>
2025-01-22 01:10:13 +0530

    (chore): move the components from deafault app.tsx to seprate pages

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./App.css";
+import Signup from "./Signup";
+import { Sidebar } from "../components/Sidebar";
+import { CreateContentModel } from "../components/CreateContentModel";
+import { Button } from "../components/Button";
+import { PlusIcon } from "../icons/PlusIcon";
+import { ShareIcon } from "../icons/ShareIcon";
+import { Card } from "../components/Card";
+
+export function Dashboard() {
+  const [modelOpen, setModelOpen] = useState(false);
+  return (
+    <div>
+      <Signup />
+      <Sidebar />
+      <div className="p-4 ml-72 min-h-screen bg-gray-100">
+        <CreateContentModel
+          open={modelOpen}
+          onClose={() => {
+            setModelOpen(false);
+          }}
+        />
+        <div className=" flex justify-end gap-4">
+          <Button
+            onClick={() => {
+              setModelOpen(true);
+            }}
+            varient="primary"
+            text="Add content"
+            startIcon={<PlusIcon />}
+          ></Button>
+
+          <Button
+            varient="secondary"
+            text="Share Brain"
+            startIcon={<ShareIcon />}
+          ></Button>
+        </div>
+
+        <div className="flex gap-4">
+          <Card
+            type="twitter"
+            link="https://twitter.com/yaseeenkc/status/1861410120858771756"
+            title="First tweet"
+          />
+
+          <Card
+            type="youtube"
+            link="https://www.youtube.com/watch?v=xYO6otF0xeM"
+            title="First video"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
