e9ca6a5e4eb18677c08e72906a9d015fed425449
Yaseen KC <ymkcmpd@gmail.com>
2025-01-22 03:13:09 +0530

    (refactor): enable try catch on Signup.tsx

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import { BACKEND_URL } from "../config";
@@ -6,20 +6,27 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export default function Signup() {
-  const usernameRef = useRef<HTMLInputElement>();
-  const passwordRef = useRef<HTMLInputElement>();
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
   const navigate = useNavigate();
 
   async function signup() {
     const username = usernameRef.current?.value;
-    console.log(usernameRef.current);
     const password = passwordRef.current?.value;
-    await axios.post(BACKEND_URL + "/api/v1/signup", {
-      username,
-      password,
-    });
-    navigate("/signin");
-    alert("You have signed up!");
+    if (!username || !password) {
+      alert("Please fill in all the fields");
+    }
+    try {
+      await axios.post(BACKEND_URL + "/api/v1/signup", {
+        username,
+        password,
+      });
+      navigate("/signin");
+      alert("You have signed up!");
+    } catch (error) {
+      console.error("Signup failed:", error);
+      alert("Signup failed. Please try again.");
+    }
   }
   return (
     <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
